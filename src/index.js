1b925db109c4d589b0ed68646c929dfb59af4f39
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-06-03 14:06:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+const form =document.getElementById("create-task-form");
+const input=document.getElementById("new-task-description");
+const div = document.getElementById("list");
+const ul = document.getElementById("tasks");
+
+form.addEventListener("submit", function make(e){
+  e.preventDefault();
+ let li=document.createElement('li');
+      li.innerHTML =input.value;
+      ul.appendChild(li);
+})
 });
